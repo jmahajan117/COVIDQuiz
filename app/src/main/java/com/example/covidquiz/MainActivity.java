6b60919e5e2a801a4fc78d273967e3e29953eed4
jmahajan117 <jay.mahajan33@gmail.com>
2021-04-11 00:00:25 -0500

    Added JDBC Library

--- app/src/main/java/com/example/covidquiz/MainActivity.java
+++ app/src/main/java/com/example/covidquiz/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.covidquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSignIn(View view) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection server = DriverManager.getConnection(
+                    "jdbc:mysql://34.122.65.95:3306/newdata",
+                    "root",
+                    "DataBased2");
+            Statement statement = server.createStatement();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
