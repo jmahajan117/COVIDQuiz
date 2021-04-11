f9ba27fcefebcaa59a226371dddfc32230177579
jmahajan117 <jay.mahajan33@gmail.com>
2021-04-11 01:02:19 -0500

    Fuck JDBC

--- app/src/main/java/com/example/covidquiz/MainActivity.java
+++ app/src/main/java/com/example/covidquiz/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,16 +19,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 
     public void onSignIn(View view) {
+        String userName = ((EditText) findViewById(R.id.userNameText)).getText().toString();
+        String password = ((EditText) findViewById(R.id.passwordText)).getText().toString();
+
         try {
+            //DONT COPY THIS CLASS SHIT
             Class.forName("com.mysql.jdbc.Driver");
+            //COPY BELOW
             Connection server = DriverManager.getConnection(
-                    "jdbc:mysql://34.122.65.95:3306/newdata",
+                    "jdbc:mysql://34.122.65.95:3306/411Data",
                     "root",
                     "DataBased2");
             Statement statement = server.createStatement();
+            //WATCH SPACES
+            ResultSet r = statement.executeQuery("SELECT u.Username, u.Password " +
+                    "FROM Users u " + "WHERE u.Username = " +
+                    "\""+ userName + "\""+  " AND u.Password = " +  "\""+ password + "\"");
+
+            //Make sure you put this loop
+            while (r.next()) {
+                if (r.getString("Username").equals(userName) && r.getString("Password").equals(password)) {
+                    Log.d("Yeet", "Works");
+                    //DO WHATEVER HERE
+                }
+            }
 
 
         } catch (Exception e) {
