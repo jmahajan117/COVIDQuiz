8009404e7977ca252cb61ff746540ee2ef0ee547
jmahajan117 <jay.mahajan33@gmail.com>
2021-04-11 03:39:15 -0500

    Worked on Questions UI

--- app/src/main/java/com/example/covidquiz/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/covidquiz/ui/dashboard/DashboardFragment.java
@@ -1,9 +1,12 @@
 package com.example.covidquiz.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,25 +15,51 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.covidquiz.Lobby;
 import com.example.covidquiz.R;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
-
+    private ListView questionList;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         getActivity().setTitle("Teams");
+        questionList = (ListView) root.findViewById(R.id.questionsList);
+
+
+        //Get Questions from SQL
+        ArrayList<String> questions = new ArrayList<>();
+        try {
+            //COPY BELOW
+            Connection server = DriverManager.getConnection(
+                    "jdbc:mysql://34.122.65.95:3306/411Data",
+                    "root",
+                    "DataBased2");
+            Statement statement = server.createStatement();
+            //WATCH SPACES
+            ResultSet r = statement.executeQuery("SELECT * FROM Questions LIMIT 15");
+
+            //Make sure you put this loop
+            while (r.next()) {
+                questions.add(r.getString("Question"));
+            }
+
+            ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(),
+                    android.R.layout.simple_expandable_list_item_1, questions);
+            questionList.setAdapter(adapter);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         return root;
     }
 }
