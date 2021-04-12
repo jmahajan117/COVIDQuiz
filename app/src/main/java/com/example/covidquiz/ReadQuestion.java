572df4246904dc5755c1a7dfd35e62f362402eba
menezes4 <menezes4@illinois.edu>
2021-04-11 23:25:21 -0500

    Delete Question works, but we need to refresh the main 'questions' page after we delete the question.

--- app/src/main/java/com/example/covidquiz/ReadQuestion.java
+++ app/src/main/java/com/example/covidquiz/ReadQuestion.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class ReadQuestion extends AppCompatActivity {
 
@@ -21,6 +26,31 @@ public class ReadQuestion extends AppCompatActivity {
 
     public void onDelete(View view) {
         //TODO: Delete the current question
+        try {
+            //COPY BELOW
+            Connection server = DriverManager.getConnection(
+                    "jdbc:mysql://34.122.65.95:3306/411Data",
+                    "root",
+                    "DataBased2");
+            Statement statement = server.createStatement();
+            //WATCH SPACES
+            int r = statement.executeUpdate("DELETE " +
+                    "FROM Questions " + "WHERE Question = " +
+                    "\""+ question + "\"");
+
+            // Notify whether deletion was successful
+            if (r == 1) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Question Successfully Deleted", Toast.LENGTH_LONG);
+                toast.show();
+            } else {
+                Toast toast = Toast.makeText(getApplicationContext(), "Operation Unsuccessful", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        finish();
     }
 
 
