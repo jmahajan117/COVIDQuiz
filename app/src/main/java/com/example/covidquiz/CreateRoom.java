5ef7244bde666f73b7ab54b938487bfaaded1ee9
menezes4 <menezes4@illinois.edu>
2021-04-12 00:46:09 -0500

    Creating a Room is done.

--- app/src/main/java/com/example/covidquiz/CreateRoom.java
+++ app/src/main/java/com/example/covidquiz/CreateRoom.java
@@ -5,8 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class CreateRoom extends AppCompatActivity {
@@ -27,7 +33,26 @@ public class CreateRoom extends AppCompatActivity {
         ArrayList<String> allTopics = new ArrayList<>();
         ArrayList<String> allPossibleTeams = new ArrayList<>();
         //TODO: Populate the spinner, for topics and teams (EXCEPT THE CURR TEAM)
+        try {
+            Connection server = DriverManager.getConnection(
+                    "jdbc:mysql://34.122.65.95:3306/411Data",
+                    "root",
+                    "DataBased2");
+            Statement statement = server.createStatement();
 
+            ResultSet r = statement.executeQuery("SELECT * " + "FROM Topics");
+            while (r.next()) {
+                allTopics.add(r.getString("TopicName"));
+            }
+
+            ResultSet r2 = statement.executeQuery("SELECT TeamName " + "FROM Teams " +
+                    "WHERE TeamName <> \"" + currentTeam + "\"");
+            while (r2.next()) {
+                allPossibleTeams.add(r2.getString("TeamName"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
         ArrayAdapter topicAdapter = new ArrayAdapter(this,
@@ -41,5 +66,39 @@ public class CreateRoom extends AppCompatActivity {
 
     public void onCreateRoom(View view) {
         // TODO: Add room to SQL
+        String roomNameDB = ((EditText) findViewById(R.id.editTextRoomName)).getText().toString();
+        String otherTeam = ((Spinner) findViewById(R.id.spinnerTeams)).getSelectedItem().toString();
+        String roomTopic = ((Spinner) findViewById(R.id.spinnerTopic)).getSelectedItem().toString();
+
+        if (currentTeam == null) {
+            Toast toast = Toast.makeText(getApplicationContext(), "You are not in a team!", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        if (currentTeam != null) {
+            try {
+                Connection server = DriverManager.getConnection(
+                        "jdbc:mysql://34.122.65.95:3306/411Data",
+                        "root",
+                        "DataBased2");
+                Statement statement = server.createStatement();
+
+                // Query
+                int r = statement.executeUpdate("INSERT INTO Rooms(RoomName, TeamA, TeamB, WinsA, WinsB, RoomTopic) " +
+                        "VALUES(" + "\"" + roomNameDB + "\", \"" + currentTeam + "\", \"" + otherTeam + "\", 0, 0, \"" + roomTopic + "\")");
+
+                if (r == 1) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Room Successfully Added", Toast.LENGTH_LONG);
+                    toast.show();
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Operation Unsuccessful", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
