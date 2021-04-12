53997ff855f80bfdee8651e4df8698c63b508d99
jmahajan117 <jay.mahajan33@gmail.com>
2021-04-11 23:20:14 -0500

    Added UI to get all of the rooms for a team

--- app/src/main/java/com/example/covidquiz/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/covidquiz/ui/notifications/NotificationsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,13 +16,26 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.covidquiz.R;
 
+import java.util.ArrayList;
+
 public class NotificationsFragment extends Fragment {
 
 
+    private ListView roomList;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        roomList = (ListView) root.findViewById(R.id.roomList);
+        ArrayList<String> rooms = new ArrayList<>();
+        String teamName = getActivity().getIntent().getStringExtra("Team Name");
+        //TODO:List all of the rooms that a team is in
+
+
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(),
+                android.R.layout.simple_list_item_1, rooms);
+        roomList.setAdapter(adapter);
+
 
         return root;
     }
