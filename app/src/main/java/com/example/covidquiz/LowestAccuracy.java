cadf05f646f663656134047345bc9af2348e9ba5
jmahajan117 <jay.mahajan33@gmail.com>
2021-04-12 04:59:33 -0500

    Updated Buttons

--- app/src/main/java/com/example/covidquiz/LowestAccuracy.java
+++ app/src/main/java/com/example/covidquiz/LowestAccuracy.java
@@ -2,10 +2,12 @@ package com.example.covidquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -19,6 +21,7 @@ import java.util.ArrayList;
 public class LowestAccuracy extends AppCompatActivity {
 
     private ListView QAList;
+    String topicDBName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,32 +29,43 @@ public class LowestAccuracy extends AppCompatActivity {
         setContentView(R.layout.activity_lowest_accuracy);
 
         QAList = (ListView) findViewById(R.id.questionAccList);
+        topicDBName = ((EditText) findViewById(R.id.editTextTopicEnter)).getText().toString();
+
+        Button b = (Button) findViewById(R.id.buttonSubmit);
+        b.setOnClickListener(v -> {
+            topicDBName = ((EditText) findViewById(R.id.editTextTopicEnter)).getText().toString();
+            new Task().execute();
+        });
     }
 
-    public void onEntry(View view) {
+    class Task extends AsyncTask<Void, Void, Void> {
         ArrayList<TwoEntry> entries = new ArrayList<>();
-        Entry2DAdapter adapter = new Entry2DAdapter(this, R.layout.entry2display, entries);
-        String topicDBName = ((EditText) findViewById(R.id.editTextTopicEnter)).getText().toString();
+        @Override
+        protected Void doInBackground(Void... voids) {
 
-        //TODO: Get the topic, display lowest acc
-        try {
-            Connection server = DriverManager.getConnection(
-                    "jdbc:mysql://34.122.65.95:3306/411Data",
-                    "root",
-                    "DataBased2");
-            Statement statement = server.createStatement();
-            //WATCH SPACES
-            ResultSet r = statement.executeQuery("SELECT Question, Accuracy FROM Questions WHERE Topic = \"" +
-                    topicDBName + "\" ORDER BY Accuracy ASC LIMIT 15");
 
-            while (r.next()) {
-                TwoEntry item = new TwoEntry(r.getString("Question"), r.getString("Accuracy"));
-                entries.add(item);
+            //TODO: Get the topic, display lowest acc
+            try {
+                Connection server = DriverManager.getConnection(
+                        "jdbc:mysql://34.122.65.95:3306/411Data",
+                        "root",
+                        "DataBased2");
+                Statement statement = server.createStatement();
+                //WATCH SPACES
+                ResultSet r = statement.executeQuery("SELECT Question, Accuracy FROM Questions WHERE Topic = \"" +
+                        topicDBName + "\" ORDER BY Accuracy ASC LIMIT 15");
+
+                while (r.next()) {
+                    TwoEntry item = new TwoEntry(r.getString("Question"), r.getString("Accuracy"));
+                    entries.add(item);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            Entry2DAdapter adapter = new Entry2DAdapter(getApplicationContext(), R.layout.entry2display, entries);
+            QAList.setAdapter(adapter);
+            return null;
         }
-
-        QAList.setAdapter(adapter);
     }
+
 }
