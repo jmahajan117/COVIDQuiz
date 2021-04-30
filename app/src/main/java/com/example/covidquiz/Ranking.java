44918057f3ee4684052af0849d06e1d01361b5b6
Matthew Jakimovski <mjakim2@illinois.edu>
2021-04-29 22:39:55 -0500

    Added in one of 4 GIFs for ranking

--- app/src/main/java/com/example/covidquiz/Ranking.java
+++ app/src/main/java/com/example/covidquiz/Ranking.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Ranking extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+
     }
+
 }
