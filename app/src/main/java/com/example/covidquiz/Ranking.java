cc31a500c7f31c1ffd54532ca61e757d2033a68b
jmahajan117 <jay.mahajan33@gmail.com>
2021-04-29 23:58:03 -0500

    Added Ranking

--- app/src/main/java/com/example/covidquiz/Ranking.java
+++ app/src/main/java/com/example/covidquiz/Ranking.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.Types;
+import java.text.DecimalFormat;
 
 public class Ranking extends AppCompatActivity {
 
@@ -32,10 +38,42 @@ public class Ranking extends AppCompatActivity {
             statement.setString(1, team);
 
             statement.execute();
-            int i = statement.getInt(2);
+            int wins = statement.getInt(2);
 
 
+            int total = 0;
+            Statement count = server.createStatement();
+            ResultSet r = count.executeQuery("SELECT COUNT(*) FROM Teams");
+            while(r.next()) {
+                total = r.getInt(1);
+            }
 
+            double percent = wins / (total + 0.0);
+
+
+            ImageView view = (ImageView) findViewById(R.id.badgeImageView);
+            TextView textView = (TextView) findViewById(R.id.textViewRank);
+            String text = textView.getText().toString();
+
+            if (percent < 0.4) {
+                Glide.with(this).load(R.drawable.bronze_medal).into(view);
+                text += " \nBronze";
+            }
+            else if (percent < 0.7) {
+                Glide.with(this).load(R.drawable.silver_medal).into(view);
+                text += " \nSilver";
+            }
+
+            else if (percent < 0.9) {
+                Glide.with(this).load(R.drawable.gold_medal).into(view);
+                text += " \nGold";
+            }
+            else {
+                Glide.with(this).load(R.drawable.platinum_medal).into(view);
+                text += " \nPlatinum";
+            }
+
+            textView.setText(text);
 
         } catch (Exception e) {
             e.printStackTrace();
